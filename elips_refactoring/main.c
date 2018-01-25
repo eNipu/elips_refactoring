466ff122d1f7e6b7e27af400040e61863910e640
Md Al-Amin Khandaker <xion.nipu@gmail.com>
2018-01-25 14:25:17 +0900

    Initial Commit

---
+++ elips_refactoring/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  elips_refactoring
+//
+//  Created by Khandaker Md. Al-Amin on 1/25/18.
+//  Copyright © 2018 ISec Lab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
